bc5a98fcf1f7bf3eb3b066707ac1ff4bd408f695
Domenic <domenic.bersch@yahoo.de>
2023-04-27 19:52:37 +0200

    Update taxonomy

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version = '0.1.0',
     author = 'Roig Lab',
     packages=find_packages(),
+    package_data={
+        'net2brain.architectures': ['taxonomy.csv']
+    },
     install_requires=[
         'flake8',
         'h5py',
