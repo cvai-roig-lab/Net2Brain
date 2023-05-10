7ee1dd3542423e27c1e04f9f274ce4d7e8df954d
t9s9 <schaumloeffel.tim@web.de>
2023-05-10 08:03:30 +0200

    add RSA test and cases

--- net2brain/tests/test_rdm_creation.py
+++ net2brain/tests/test_rdm_creation.py
@@ -1,18 +1,21 @@
 import numpy as np
-
+import pytest
 from net2brain.rdm_creation import RDMCreator
 
 
-def test_rdm_creator(root_path, tmp_path):
-    data_path = root_path / "compare_files"
-    rdm = RDMCreator(
-        feat_path=str(data_path / "correct_data_feats"),
-        save_path=str(tmp_path)
-    )
+@pytest.fixture(params=["case1", "case2"])
+def case(request):
+    return request.param
+
+
+def test_rdm_creator(root_path, tmp_path, case):
+    data_path = root_path / "test_cases" / case
+
+    rdm = RDMCreator(feat_path=str(data_path / "features"), save_path=str(tmp_path))
     rdm.create_rdms()
 
     # Compare extractions with ground truth
-    gt_path = data_path / "correct_data_rdm"
+    gt_path = data_path / "rdm"
 
     for gt_file in gt_path.glob("*.npz"):
         test_file = tmp_path / gt_file.name
