23ea3b3bf899c3250dc47277c21aa2c813c0e381
t9s9 <schaumloeffel.tim@web.de>
2023-05-13 18:22:44 +0200

    bug fix

--- net2brain/tests/test_evaluation.py
+++ net2brain/tests/test_evaluation.py
@@ -16,15 +16,17 @@ from net2brain.evaluations.rsa import RSA
 def test_rsa(root_path, case, model_name):
     data_path = root_path / "test_cases"
 
-    brain_path = root_path.parent.parent / Path(
-        "notebooks", "input_data", "brain_data", "78images"
-    )
+    brain_path = root_path / Path("data", "brain_data")
     rsa = RSA(
         brain_rdms_path=brain_path,
         model_rdms_path=str(data_path / case / "rdm"),
         model_name=model_name,
     )
-    df = rsa.evaluate()
+    df = rsa.evaluate().sort_values(by=["ROI", "Layer"]).reset_index(drop=True)
 
-    gt = pd.read_csv(data_path / case / "rsa" / "results.csv")
+    gt = (
+        pd.read_csv(data_path / case / "rsa" / "results.csv")
+        .sort_values(by=["ROI", "Layer"])
+        .reset_index(drop=True)
+    )
     pd.testing.assert_frame_equal(df, gt)
