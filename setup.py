3d3f61dc237e5806d40415eaf3bb3532668e4e61
Domenic <67119745+ToastyDom@users.noreply.github.com>
2024-11-04 10:52:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'net2brain',
-    version = '0.1.0',
+    version = '1.1.2',
     author = 'Roig Lab',
     packages=find_packages(),
     package_data={
