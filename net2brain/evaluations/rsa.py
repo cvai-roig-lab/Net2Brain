9ff21e9046385916217374d8407b99ee597cc7b5
yvette <yvette-werner@web.de>
2026-01-24 19:29:46 +0100

    unwrap single-item list to extract r_array

--- net2brain/evaluations/rsa.py
+++ net2brain/evaluations/rsa.py
@@ -337,7 +337,7 @@ class RSA():
             model_ii = np.argmin([layer_dict[r_name] for layer_dict in model_layers_dict])
             other_ii = np.argmin([layer_dict[r_name] for layer_dict in other_layers_dict])
 
-            tstat, p = stats.ttest_ind(other_layers_dict[other_ii][r_array_name], model_layers_dict[model_ii][r_array_name])
+            tstat, p = stats.ttest_ind(other_layers_dict[other_ii][r_array_name][0], model_layers_dict[model_ii][r_array_name][0])
 
             scan_key = "(" + str(counter) + ") " + roi[:-4]
 
